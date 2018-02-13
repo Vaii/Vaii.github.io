846914d23d0a48ad122de6760cc3fc9137b9bdfe
Vai <vaivanommeren@gmail.com>
2018-02-14 00:33:30 +0100

    added animation for logo

--- script/frontpage.js
+++ script/frontpage.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('div.container').hide().fadeIn(2000);
 });
+
+$(document).ready(function(){
+    $('div.logo').animate({
+        height: '150px',
+        width: '550px'
+    }, 1500);
+});
